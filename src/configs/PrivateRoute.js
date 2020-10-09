02cfe94ed52808aac573016eb2c864576723776d
Cai Li <1981824361@qq.com>
2020-10-09 14:15:46 +0800

    注释说明

--- src/configs/PrivateRoute.js
+++ src/configs/PrivateRoute.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: Li Cai
+ * @LastEditors: Li Cai
+ * @Connect: 1981824361@qq.com
+ * @Date: 2020-09-03 14:25:30
+ * @LastEditTime: 2020-10-09 13:54:03
+ * @Description: 路由鉴权
+ * @FilePath: /my-app/src/configs/PrivateRoute.js
+ */
 
-import React, {Component} from 'react';
+import React from 'react';
 import {Route} from 'react-router-dom'
 // import httpUtils from "../commons/utils/FeatchUtils";
 // import {isLocalhost, cache, cacheSession} from "../commons/utils/CommonUtils";
@@ -8,7 +17,7 @@ import {Route} from 'react-router-dom'
 import {Spin} from 'antd';
 
 
-class PrivateRoute extends Component {
+class PrivateRoute extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
@@ -16,8 +25,32 @@ class PrivateRoute extends Component {
         }
     }
 
-    componentWillMount() {
-    }
+  //   componentWillMount(){
+  //     let urlParams = queryString.parse(this.props.location.search);
+  //     if(urlParams && urlParams._s){
+  //         cache.clear('Authorization');
+  //         cache.clear('Right');
+  //         cache.clear('_s');
+  //         cache.clear('authHeader');
+  //         let url = isLocalhost ? '' : host;
+  //         httpUtils.get(url+basicUrl+"/checkToken?_s="+urlParams._s+'&AppCode='+defaultAppCode.toString()).then(res => {
+  //             if(res.success){
+  //                 cacheSession.set('Authorization',res.data[0]);
+  //                 cache.set('Right',res.data[1]);
+  //                 cache.set('_s',urlParams._s);
+  //                 this.setState({hasToken:res.success});
+  //             }else {
+  //                 // window.location.replace(host + basicUrl + "/index");
+  //             }
+  //         })
+  //     }else {
+  //         if(cacheSession.get('Authorization')){
+  //             this.setState({hasToken:true});
+  //         }else {
+  //             // window.location.replace(host + basicUrl + "/login");
+  //         }
+  //     }
+  // }
 
     render() {
         const {component: Component, ...rest} = this.props
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,3 +1,12 @@
+/*
+ * @Author: Li Cai
+ * @LastEditors: Li Cai
+ * @Connect: 1981824361@qq.com
+ * @Date: 2020-09-04 14:52:57
+ * @LastEditTime: 2020-10-09 13:51:43
+ * @Description:  配置代理
+ * @FilePath: /my-app/src/setupProxy.js
+ */
 // https://blog.csdn.net/balics/article/details/104479641
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
