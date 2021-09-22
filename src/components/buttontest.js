54cd260dfab2956555f2b8ed4f02d52698517433
LiCai <1981824361@qq.com>
2021-09-22 15:15:50 +0800

    electron 配置

--- src/components/buttontest.js
+++ src/components/buttontest.js
@@ -1,15 +1,34 @@
-import React from 'react'
-import { Button } from 'antd';
+/*
+ * @Author       : LiCai
+ * @connect      : 1981824361@qq.com
+ * @Date         : 2021-01-11 17:56:53
+ * @LastEditors  : LiCai
+ * @LastEditTime : 2021-09-17 10:02:13
+ * @Description  : TODO
+ * @FilePath     : /CRA-WEB/src/components/buttontest.js
+ */
+import React from "react";
+import { Button } from "antd";
 
 class Mudle extends React.Component {
-constructor(props) {
-super(props);
-this.state = {}
+  state = {
+    count: 0,
+  };
+
+  componentDidMount() {
+    for (let index = 0; index < 3; index++) {
+      setTimeout(() => {
+        this.setState({ count: this.state.count + 1 }, () => {
+            console.log(index, this.state.count);
+          });
+      }, 0);
+    }
+  }
+
+  render() {
+      console.log(this.state.count);
+    return <Button type='primary'>测试</Button>;
+  }
 }
 
-render() {
-return <Button type="primary">测试</Button>;
-}
-}
-
-export default (Mudle);
+export default Mudle;
--- src/components/common/MultiCascaderCom.js
+++ src/components/common/MultiCascaderCom.js
@@ -3,7 +3,7 @@
  * @connect      : 1981824361@qq.com
  * @Date         : 2021-01-12 10:22:43
  * @LastEditors  : LiCai
- * @LastEditTime : 2021-01-12 13:39:49
+ * @LastEditTime : 2021-09-18 16:04:49
  * @Description  : 级联组件
  * @FilePath     : /CRA-WEB/src/components/common/MultiCascaderCom.js
  */
@@ -14,6 +14,7 @@ import MultiCascader from "antd-multi-cascader";
 import "antd/dist/antd.css";
 import "antd-multi-cascader/dist/index.css";
 import options from "./options";
+import { Link } from "react-router-dom";
 
 export default function App() {
   const [value, setValue] = React.useState(["福田区", "罗湖区"]);
@@ -56,6 +57,7 @@ export default function App() {
           selectAllText="全选"
         />
       </section>
+      <Link to='/Virtable'>测试</Link>
     </div>
   );
 }
